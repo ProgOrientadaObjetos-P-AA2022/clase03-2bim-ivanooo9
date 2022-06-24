6639d5512fc01ae0bf9fb4b3c5fef5d01ecdb6f2
ivanooo9 <marcoeli098@gmail.com>
2022-06-24 18:59:19 -0500

    Clase03

--- EjemploPolimorfismo/Ejemplopolimorfismo001/src/paquete05/EjecutaEstudiante.java
+++ EjemploPolimorfismo/Ejemplopolimorfismo001/src/paquete05/EjecutaEstudiante.java
@@ -1,9 +1,8 @@
-
 package paquete05;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import paquete04.Estudiante;
+import paquete04.*;
 
 public class EjecutaEstudiante {
 
@@ -16,7 +15,7 @@ public class EjecutaEstudiante {
         de tipo EstudiantePresencial y EstudianteDistancia
         quiere ingresar.
         
-        */
+         */
         Scanner entrada = new Scanner(System.in);
         String nombresEst;
         String apellidosEst;
@@ -26,27 +25,101 @@ public class EjecutaEstudiante {
         int numeroCreds;
         double costoAsig;
         int numeroAsigs;
-        int tipoEstudiante;
+        int tipoEstudiante = 0;
         String continuar;
-        int contador;
+       
         ArrayList<Estudiante> estudiantes = new ArrayList<>();
-        
+
         // inicio de solución
         
-        
-        
-        
-        
+
+        do {
+            System.out.println("Tipo de Estudiante a ingresar\n"
+                    + "Ingrese (1) para Estudiante Presencial\n"
+                    + "Ingrese (2) para Estudiante Distancia\n");
+            tipoEstudiante = Integer.parseInt(entrada.nextLine());
+            System.out.println("Ingrese los nombres del estudiante");
+            nombresEst = entrada.nextLine();
+            System.out.println("Ingrese los apellidos del estudiante");
+            apellidosEst = entrada.nextLine();
+            System.out.println("Ingrese la identificación del estudiante");
+            identificacionEst = entrada.nextLine();
+            System.out.println("Ingrese la edad del estudiante");
+            edadEst = Integer.parseInt(entrada.nextLine());
+            if (tipoEstudiante == 1) {
+
+                // Declarar,crear e iniciar objeto tipo EstudiantePresencial
+                EstudiantePresencial estudianteP = new EstudiantePresencial();
+                // Solicitar ingreso de valores para variables 
+                // Solicitar numeroCreds, costoCred
+                // Leer numeroCreds, costoCred
+                System.out.println("Ingrese el número de créditos");
+                numeroCreds = Integer.parseInt(entrada.nextLine());
+                System.out.println("Ingrese el costo de cada créditos");
+                costoCred = Double.parseDouble(entrada.nextLine());
+                // se hace uso de los métodos establecer para asignar valores
+                // a los datos (atributos) del objeto
+                estudianteP.establecerNombresEstudiante(nombresEst);
+                estudianteP.establecerApellidoEstudiante(apellidosEst);
+                estudianteP.establecerIdentificacionEstudiante(identificacionEst);
+                estudianteP.establecerEdadEstudiante(edadEst);
+                estudianteP.establecerNumeroCreditos(numeroCreds);
+                estudianteP.establecerCostoCredito(costoCred);
+                // Se agrega al arreglo estudiantes un objeto de tipo
+                // EstudiantePresencial
+//                estudiantes[contador] = estudianteP;
+                estudiantes.add(estudianteP);
+
+            } else {
+                if (tipoEstudiante == 2) {
+
+                    EstudianteDistancia estudianteD = new EstudianteDistancia();
+                    // Solicitar ingreso de valores para variables 
+                    // Solicitar numeroAsigs, costoAsig 
+                    // Leer numeroAsigs, costoAsig
+                    System.out.println("Ingrese el número de asignaturas");
+                    numeroAsigs = Integer.parseInt(entrada.nextLine());
+                    System.out.println("Ingrese el costo de cada cada asignatura");
+                    costoAsig = Double.parseDouble(entrada.nextLine());
+
+                    // se hace uso de los métodos establecer para asignar valores
+                    // a los datos (atributos) del objeto
+                    estudianteD.establecerNombresEstudiante(nombresEst);
+                    estudianteD.establecerApellidoEstudiante(apellidosEst);
+                    estudianteD.establecerIdentificacionEstudiante(identificacionEst);
+                    estudianteD.establecerEdadEstudiante(edadEst);
+                    estudianteD.establecerNumeroAsginaturas(numeroAsigs);
+                    estudianteD.establecerCostoAsignatura(costoAsig);
+
+                    // Se agrega al arreglo estudiantes un objeto de tipo
+                    // EstudianteDistancia
+                    estudiantes.add(estudianteD);
+
+                }else{
+                    System.out.println("Fuera de rango");
+                }
+            }
+            System.out.println("Desea ingresar más estudiante. Digite la "
+                    + "letra S para continuar o digite la letra N para salir "
+                    + "del proceso");
+
+            // se captura el valor ingresado por el usuario para la variable 
+            // continuar
+            continuar = entrada.nextLine();
+
+        } while (continuar.equalsIgnoreCase("S"));
+
         // ciclo que permite comprobar el polimorfismo
         // este código no debe ser modificado.
         for (int i = 0; i < estudiantes.size(); i++) {
             // 1.  
             estudiantes.get(i).calcularMatricula();
-            
-            System.out.printf("Datos Estudiante\n"
-                        + "%s\n",                        
-                  estudiantes.get(i));
-            
+
+            System.out.printf("Datos Estudiante :"
+                    + "%s\n"
+                    + "%s\n",(i+1),
+                    estudiantes.get(i));
+
         }
     }
 
---
+++ EjemploPolimorfismo/Ejemplopolimorfismo001/src/paquete05/EstudianteDistancia.java
@@ -0,0 +1,55 @@
+package paquete05;
+
+import paquete04.*;
+
+public class EstudianteDistancia extends Estudiante {
+
+    /*1.  Declarar
+        numeroAsignaturas: Entero 
+        costoAsginatura: Real 
+     */
+
+    int numeroAsignaturas;
+    double costoAsignatura;
+
+    //  Métodos establecer y calcular para los datos o atributos de la clase
+    // 2.  Método establecerNumeroAsginaturas(numero: Real)
+    public void establecerNumeroAsginaturas(int numero) {
+        numeroAsignaturas = numero;
+    }
+
+    // 3.  Método establecerCostoAsignatura(valor: Real)
+    public void establecerCostoAsignatura(double valor) {
+        costoAsignatura = valor;
+    }
+
+    // 4.  Método calcularMatriculaDistancia()
+    @Override
+    public void calcularMatricula() {
+        matricula = numeroAsignaturas * costoAsignatura;
+    }
+
+    //  Métodos obtener para los datos o atributos de la clase
+    // 5. Método obtenerNumeroAsignaturas() : Entero
+    public int obtenerNumeroAsignaturas() {
+        return numeroAsignaturas;
+    }
+
+    // 6. Método obtenerCostoAsignatura() : Real
+    public double obtenerCostoAsignatura() {
+        return costoAsignatura;
+    }
+
+    @Override
+    public String toString() {
+        String cadena = String.format(super.toString());
+        cadena = String.format("Estudiante Distancia\n"
+                + "%s\n"
+                + "Numero Asignatura:%d\n"
+                + "Costo Asignatura:%.2f\n", cadena,
+                numeroAsignaturas, costoAsignatura);
+        return cadena;
+
+    }
+
+}
